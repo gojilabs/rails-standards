52ee888b4cd195c9e1b5bab107e5971296a87921
Bohdan Malets <bohdan.malets@gmail.com>
2023-11-10 23:03:14 +0100

    run callback from worker pod only

--- config/initializers/sidekiq_alive.rb
+++ config/initializers/sidekiq_alive.rb
@@ -3,13 +3,16 @@
 if defined?(Sidekiq) && defined?(SidekiqAlive)
   SidekiqAlive.setup do |config|
     config.callback = proc do
-      Sidekiq::Queue.all.each do |queue|
-        # skip other queues (default, critical, etc.)
-        next unless queue.name.starts_with?(config.queue_prefix.to_s)
-        # keep actual sidekiq-alive-<hostname> queue
-        next if queue.name == "#{config.queue_prefix}-#{SidekiqAlive.hostname}"
+      # execute this only from worker pod
+      if SidekiqAlive.hostname.starts_with?('worker')
+        Sidekiq::Queue.all.each do |queue|
+          # skip other queues (default, critical, etc.)
+          next unless queue.name.starts_with?(config.queue_prefix.to_s)
+          # keep actual sidekiq-alive-<hostname> queue
+          next if queue.name == "#{config.queue_prefix}-#{SidekiqAlive.hostname}"
 
-        queue.clear
+          queue.clear
+        end
       end
     end
   end
