3bb9de22329f93545fc0c5251a53a462ac83384c
Bohdan Malets <bohdan.malets@gmail.com>
2023-11-10 23:28:40 +0100

    one more try

--- config/initializers/sidekiq_alive.rb
+++ config/initializers/sidekiq_alive.rb
@@ -2,18 +2,8 @@
 
 if defined?(Sidekiq) && defined?(SidekiqAlive)
   SidekiqAlive.setup do |config|
-    config.callback = proc do
-      # execute this only from worker pod
-      if SidekiqAlive.hostname.starts_with?('worker')
-        Sidekiq::Queue.all.each do |queue|
-          # skip other queues (default, critical, etc.)
-          next unless queue.name.starts_with?(config.queue_prefix.to_s)
-          # keep actual sidekiq-alive-<hostname> queue
-          next if queue.name == "#{config.queue_prefix}-#{SidekiqAlive.hostname}"
-
-          queue.clear
-        end
-      end
+    config.shutdown_callback = proc do
+      Sidekiq::Queue.all.find { |q| q.name == "#{config.queue_prefix}-#{SidekiqAlive.hostname}" }&.clear
     end
   end
 end
