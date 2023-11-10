09952541c6233da14a2911aabf1fc639a651ac4a
Bohdan Malets <bohdan.malets@gmail.com>
2023-11-10 21:31:19 +0100

    convert queue name from symbol to string

--- config/initializers/sidekiq_alive.rb
+++ config/initializers/sidekiq_alive.rb
@@ -3,7 +3,7 @@
 if defined?(Sidekiq) && defined?(SidekiqAlive)
   SidekiqAlive.setup do |config|
     config.shutdown_callback = proc do
-      Sidekiq::Queue.all.each { |queue| queue.clear if queue.name.starts_with?(config.queue_prefix) }
+      Sidekiq::Queue.all.each { |queue| queue.clear if queue.name.starts_with?(config.queue_prefix.to_s) }
     end
   end
 end
